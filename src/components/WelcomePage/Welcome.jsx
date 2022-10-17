2d3454d9b9228d1739b6692440085d10f55608a6
KASHI TAMANG <tamang.kashi@gmail.com>
2022-10-17 10:10:43 -0700

    adding button to Welcome

--- src/components/WelcomePage/Welcome.jsx
+++ src/components/WelcomePage/Welcome.jsx
@@ -1,14 +1,23 @@
-import { Outlet, Navigate, Link } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from './Welcome.module.scss';
 import { useUser } from '../../state/UserContext';
+import CustomButton from '../Buttons/CustomButton';
 
 export default function Welcome() {
   const user = useUser();
   if (user) return <Navigate to="/" />;
+
+  const navigate = useNavigate(); 
+
+  const getStarted = () => { 
+    navigate('signup');
+  };
+
   return (
     <main className={styles.Welcome}>
       <h1>Welcome Page</h1>
-      <Link to="signup">Sign up</Link>
+      <CustomButton onClick={getStarted}>get started</CustomButton>
       <Outlet />
     </main>
   );
