604df0fd96eb53510fb4b8e3ca4465d638a65af2
austinbhan <austinbhan@outlook.com>
2022-10-18 09:53:37 -0700

    footer images

--- src/components/Page/Navigation.jsx
+++ src/components/Page/Navigation.jsx
@@ -1,17 +1,20 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.scss';
+import iconHome from '../../assets/icon_home.png';
+import iconProfile from '../../assets/icon_profile.png';
+import iconAbout from '../../assets/icon_about.png';
 
 export default function Navigation() {
   return (
     <nav className={styles.Navigation}>
       <NavLink to="">
-        home
+        <img src={iconHome}/>
       </NavLink>
       <NavLink to="profile"> 
-        profile
+        <img src={iconProfile}/>
       </NavLink>
       <NavLink to="about">
-        info
+        <img src={iconAbout}/>
       </NavLink>
     </nav>
   );
