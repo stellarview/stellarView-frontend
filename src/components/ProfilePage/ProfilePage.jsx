544286156f3222f12b63ac5eabe9775c82ff288a
KASHI TAMANG <tamang.kashi@gmail.com>
2022-10-18 17:13:10 -0700

    adding ProfilePage functionality and CompletedCategoriesList

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,23 +1,32 @@
 import styles from './ProfilePage.module.scss';
 import { useEffect, useState } from 'react';
 import { getUserById } from '../../services/users';
-import UserInfoList from './UserInfoList';
+import CompletedCategoriesList from './UserInfoList';
 
 export default function ProfilePage() {
-  const [userInfo, setUserInfo] = useState([]);
+  const [userInfo, setUserInfo] = useState({ completed_categories: [] });
 
   async function fetchUserInfo() {
-    const userData = await getUserById();
-    setUserInfo(userData);
+    const results = await getUserById();
+    console.log('results', results);
+    setUserInfo(results.data);
   }
 
   useEffect(() => {
     fetchUserInfo();
   }, []);
 
+  const getBadges = (badges) => {
+    if (badges.length > 0) {
+      return <CompletedCategoriesList category={badges} />;
+    } 
+  };
+
   return (
     <div className={styles.ProfilePage}>
-      <UserInfoList userInfo={userInfo} />
+      <h1>{`Welcome, ${userInfo.username}!`}</h1>
+      <p>points: {userInfo.total_points}</p>
+      {getBadges(userInfo.completed_categories)}
     </div>
   );
 }
--- src/components/ProfilePage/UserInfoList.jsx
+++ src/components/ProfilePage/UserInfoList.jsx
@@ -1,31 +1,9 @@
-export default function UserInfoList({ userInfo }) {
-  return (
-    <div>
-      <h1>User Info will be displayed below</h1>
-      {userInfo.map((user, i) => (
-        <ul key={user.userInfo + i}>
-          <li>{user.username}</li>
-          <li>{user.total_points}</li>
-          <li>{user.completed_categories}</li>
-        </ul>
-      ))}
-    </div>
-  );
-}
+export default function CompletedCategoriesList({ category }) {
+  // const { completed_categories } = userInfo;
 
-{
-  /* <div className={styles.UserInfo}>
-        <h1>User Info</h1>
-        <h3>username</h3>
-        <h3>date joined</h3>
-        <h3>points?</h3>
-        
-      </div>
-      <div className={styles.Badges}> 
-        <h1>Badges</h1>
-        <ul><li>html level 1(badge)</li>
-          <li>html level 2(badge)</li>
-          <li>css level 1(badge)</li>
-        </ul>
-      </div> */
+  return (
+    <ul>
+      <li>{category}</li>
+    </ul>
+  );
 }
